6436c6693f07e0ffbb850c7f9aca369316bcf842
Dave Bryan <dave.tamakloe@gmail.com>
2024-08-16 15:03:07 +0000

    test challenge covering multiple concepts

---
+++ challenge.py
@@ -0,0 +1,30 @@
+import json
+
+# Define the file path
+file = 'names.json'
+
+# Initialize the name variable
+name = ""
+
+# Try to open the file and load the name from it
+try:
+    open(file, "r")  # Open the file for reading
+    name = json.load(open(file, "r"))  # Load the name from the file
+except IOError:
+    print("User doesn't exist")
+
+# Check if the name is not empty
+if name != "":
+    print("Hello " + name)
+else:
+    name = input("What is your name? ")  # Prompt the user for their name
+    print("Hello " + name)
+    json.dump(name, open(file, "w"))  # Save the name to the file
+
+
+
+
+
+
+
+
